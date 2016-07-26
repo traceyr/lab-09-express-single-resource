796cdcba7d91841c123ab2ac586670a54e0b2ac6
Tracey <radcliffe.tracey@gmail.com>
2016-07-26 12:21:27 -0700

    did some work, not finished

---
+++ lab-traceyr/server.js
@@ -0,0 +1,11 @@
+'use strict';
+// require('./model/constructor');
+const express = require('express');
+const Router = express.Router('/api/');
+let app = express();
+
+app.post()
+
+module.exports = exports = app.listen(3000, function(){
+  console.log('Server Is On');
+});
