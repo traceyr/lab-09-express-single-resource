a76a98f0cdae2e2c27dfe6240fe3293119cffe44
Tracey <radcliffe.tracey@gmail.com>
2016-07-31 17:59:34 -0700

    fixed indentation errors that travis caught but linter did not

--- lab-traceyr/test/test_server.js
+++ lab-traceyr/test/test_server.js
@@ -15,80 +15,80 @@ describe('Testing', function(){
 
   it('should return a 404 for POST', function(done){
     request(server)
-    .post('/api/coffee')
-    .send({
-      nothing: 'not right at all'
-    })
-    .end(function(err, res){
-      expect(res.status).to.eql(400);
-      expect(res.text).to.have.string('bad request');
-      done();
-    });
+      .post('/api/coffee')
+      .send({
+        nothing: 'not right at all'
+      })
+      .end(function(err, res){
+        expect(res.status).to.eql(400);
+        expect(res.text).to.have.string('bad request');
+        done();
+      });
   });
 
   it('should return a 200 for POST', function(done){
     request(server)
-    .post('/api/coffee')
-    .send({
-      name: 'Americano'
-    })
-    .end(function(err, res){
-      expect(res.status).to.eql(200);
-      expect(res.text).to.have.string('Americano');
-      done();
-    });
+      .post('/api/coffee')
+      .send({
+        name: 'Americano'
+      })
+      .end(function(err, res){
+        expect(res.status).to.eql(200);
+        expect(res.text).to.have.string('Americano');
+        done();
+      });
   });
 
   it('should return a 200 for GET', function(done){
     request(server)
-    .get('/api/coffee/1234')
-    .end(function(err, res){
-      expect(err).to.eql(null);
-      expect(res.status).to.eql(200);
-      expect(res.text).to.have.string('cappuchino');
-      done();
-    });
+      .get('/api/coffee/1234')
+      .end(function(err, res){
+        expect(err).to.eql(null);
+        expect(res.status).to.eql(200);
+        expect(res.text).to.have.string('cappuchino');
+        done();
+      });
   });
 
   it('should return a 400 for GET', function(done){
     request(server)
-    .get('/api/coffee')
-    .end(function(err, res){
-      expect(res.status).to.eql(400);
-      expect(res.text).to.have.string('bad request');
-      done();
-    });
+      .get('/api/coffee')
+      .end(function(err, res){
+        expect(res.status).to.eql(400);
+        expect(res.text).to.have.string('bad request');
+        done();
+      });
   });
 
   it('should return a 404 for GET', function(done){
     request(server)
-    .get('/api/coffee/6543')
-    .end(function(err, res){
-      expect(res.status).to.eql(404);
-      expect(res.text).to.have.string('not found');
-      done();
-    });
+      .get('/api/coffee/6543')
+      .end(function(err, res){
+        expect(res.status).to.eql(404);
+        expect(res.text).to.have.string('not found');
+        done();
+      });
   });
 
   it('should return a 400 for PUT', function(done){
     request(server)
-    .put('/api/coffee/3211?name=ralph')
-    .end(function(err, res){
-      expect(res.status).to.eql(400);
-      expect(res.text).to.have.string('bad request');
-      done();
-    });
+      .put('/api/coffee/3211?name=ralph')
+      .end(function(err, res){
+        expect(res.status).to.eql(400);
+        expect(res.text).to.have.string('bad request');
+        done();
+      });
   });
 
   it('should return a 200 for PUT', function(done){
     request(server)
-    .put('/api/coffee/1234')
-    .send({name: 'tea'})
-    .end(function(err, res){
-      expect(res.status).to.eql(200);
-      expect(res.text).to.have.string('tea');
-      done();
-    });
+      .put('/api/coffee/1234')
+      .send({name: 'tea'})
+      .end(function(err, res){
+        expect(res.status).to.eql(200);
+        expect(res.text).to.have.string('tea');
+        done();
+      });
   });
 
 });
